f329450a49bd4a9a853e33cb3f3973ba630f584f
Jason <jason.addie@gmail.com>
2013-01-16 10:59:05 +0400

    added google analytics

--- timeline/send_mail.php
+++ timeline/send_mail.php
@@ -7,11 +7,11 @@ $email = $_POST['email'];
 
 $message="Date: $date \n Event: \n $event \n Link: $link \n Name: $name \n Email: $email";
 
-$subject ="Georgian Timeline Event Submission"; 
+$subject ="Georgian Timeline Event Submission";
 $to ='jason.addie@jumpstart.ge';
 $header="from: $to";
 
-$send_contact=mail($to,$subject,$message,$header);
+#$send_contact=mail($to,$subject,$message,$header);
 
 if($send_contact){
 echo true;
