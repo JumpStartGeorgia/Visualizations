fd7d1a2cc3fc916605f769866cdf8399910e39d6
Jason <jason.addie@gmail.com>
2013-01-16 12:34:17 +0400

    got php email to work

--- timeline/send_mail.php
+++ timeline/send_mail.php
@@ -1,22 +1,45 @@
 <?php
+require_once './lib/swift_required.php';
+
 $event = $_POST['event'];
 $date = $_POST['date'];
 $link = $_POST['link'];
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-$message="Date: $date \n Event: \n $event \n Link: $link \n Name: $name \n Email: $email";
+$message=" Date: $date \n\n Event: \n $event \n\n Link: $link \n\n Name: $name \n\n Email: $email";
 
 $subject ="Georgian Timeline Event Submission";
-$to ='jason.addie@jumpstart.ge';
+$to ="info@feradi.info";
+$pwd ="IdareFIdareF";
 $header="from: $to";
 
-#$send_contact=mail($to,$subject,$message,$header);
+$email = Swift_Message::newInstance()
+  ->setSubject($subject)
+  ->setFrom($to)
+  ->setTo($to)
+  ->setBody($message)
+;
 
-if($send_contact){
-echo true;
+$transport = Swift_SmtpTransport::newInstance('ssl://smtp.gmail.com', 465)
+  ->setUsername($to)
+  ->setPassword()
+  ;
+
+$mailer = Swift_Mailer::newInstance($transport);
+$result = $mailer->send($email);
+
+#$send_contact = mail($to,$subject,$message,$header);
+
+if($result){
+  echo json_encode(array(
+      'status' => 'success'
+  ));
 }
 else {
-echo false;
+  echo json_encode(array(
+      'status' => 'error'
+  ));
 }
+exit();
 ?>
