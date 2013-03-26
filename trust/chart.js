a0e98249505dc6c0b463188df175d5733fe23887
jason <jason.addie@gmail.com>
2013-03-26 14:51:38 +0400

    started trust visual

---
+++ trust/chart.js
@@ -0,0 +1,54 @@
+$(function () {
+        $('#container').highcharts({
+            chart: {
+                type: 'column',
+                margin: [ 50, 50, 100, 80]
+            },
+            title: {
+                text: 'World\'s largest cities per 2008'
+            },
+            xAxis: {
+                categories: create_chart_axis(chart_data['trust']['axis']),
+                labels: {
+                    rotation: -45,
+                    align: 'right',
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Population (millions)'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                formatter: function() {
+                    return '<b>'+ this.x +'</b><br/>'+
+                        'Population in 2008: '+ Highcharts.numberFormat(this.y, 1) +
+                        ' millions';
+                }
+            },
+            series: [{
+                name: 'Population',
+                data: chart_data['trust']['values'],
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    x: 4,
+                    y: 10,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            }]
+        });
+    });
---
+++ trust/data.js
@@ -0,0 +1,90 @@
+var locale = 'ka';
+
+function create_chart_axis(axis){
+console.log('create chart axis start');
+  x = [];
+
+  for (var i = 0; i<axis.length; i++){
+    console.log("axis[i] = " + axis[i]);
+    x[i] = translations[locale]['categories'][axis[i]];
+  }
+
+console.log('create chart axis stop');
+  return x;
+}
+
+var chart_data = {
+  trust: {
+    axis: ["rel", "army", "police", "ex_gov", "ed", "parl", "eu", "un", "health", "banks", "omb", "local_gov", "pres", "ngo", "media", "courts", "pol_parties"],
+    values: [85,68,50,48,44,43,41,40,39,37,32,28,28,25,22,19,16]
+  },
+  distrust: {
+    axis: [],
+    values: [31,26,20,18,17,17,14,11,10,9,8,7,6,6,4,4,2]
+  },
+  partial: {
+    axis: [],
+    values: [51,44,40,38,33,31,31,31,31,29,28,28,28,28,27,18,9]
+  },
+  not_know: {
+    axis: [],
+    values: [35,33,27,25,25,23,18,17,16,15,14,13,10,9,8,6,5]
+  }
+};
+
+var translations = {
+  en: {
+    categories:{
+      army: "Army",
+      banks: "Banks",
+      courts: "Court System",
+      ed: "Educational System",
+      eu: "European Union",
+      ex_gov: "Executive Government",
+      health: "Healthcare System",
+      local_gov: "Local Government",
+      media: "Media",
+      ngo: "NGOs",
+      omb: "Ombudsman",
+      parl: "Parliament",
+      police: "Police",
+      pol_parties: "Political Parties",
+      pres: "President",
+      rel: "Religious Institutions",
+      un: "United Nation"
+    },
+    groups:{
+      trust: "Trust",
+      distrust: "Distrust",
+      partial: "Partialy Both",
+      now_know: "Don't Know"
+    }
+  },
+  ka: {
+    categories:{
+      army: "ჯარი",
+      banks: "ბანკები",
+      courts: "სასამართლო სისტემა",
+      ed: "საგანმანათლებლო სისტემა",
+      eu: "ევროკავშირი",
+      ex_gov: "აღმასრულებელი ხელისუფლება",
+      health: "ჯანდაცვის სისტემა",
+      local_gov: "ადგილობრივი ხელისუფლება",
+      media: "მასმედია",
+      ngo: "არასამთავრობო ორგანიზაციები",
+      omb: "სახალხო დამცველი",
+      parl: "პარლამენტი",
+      police: "პოლიცია",
+      pol_parties: "პარტიები",
+      pres: "პრეზიდენტი",
+      rel: "რელიგიური ინსტიტუტები",
+      un: "გაერო"
+    },
+    groups:{
+      trust: "ვენდობი",
+      distrust: "არ ვენდობი",
+      partial: "ნაწილობრივ ორივე",
+      now_know: "არ ვიცი"
+    }
+  }
+};
