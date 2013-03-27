d3cb6fa28db1cd48513887505d775efbf46e0351
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-03-27 17:13:41 +0400

    new file included

---
+++ trust/tooltip.js
@@ -0,0 +1,44 @@
+function build_tooltip (data)
+{
+  
+  $('#tooltip').highcharts({
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Monthly Average Rainfall'
+      },
+      subtitle: {
+        text: 'Source: WorldClimate.com'
+      },
+      xAxis: {
+        categories: data.categories
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Rainfall (mm)'
+        }
+      },
+      tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+          '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: [{
+        name: 'Tokyo',
+        data: data.values
+      }]
+  });
+  };
+
+
