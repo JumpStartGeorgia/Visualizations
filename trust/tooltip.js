118f9d89d92f3f94a039d5fb9ec6fc93266d68d0
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-03-28 16:48:07 +0400

    working on tooltip position

--- trust/tooltip.js
+++ trust/tooltip.js
@@ -51,8 +51,16 @@ function build_tooltip (data)
   tooltip_options.xAxis.categories = data.categories;
   tooltip_options.title.text = data.title;
   tooltip_options.series[0].data = data.values;
-  var top = $('#main_chart').height() + $('#main_chart').position().top;
+  
+  var top = $('#main_chart').height() + $('#main_chart').offset().top;
   var left = data.position.x - $('#tooltip').width() / 2;
+
+  if (left + $('#tooltip .chart').width() > $(window).width())
+  {
+    //$('#tooltip .chart').css({position: 'absolute', left: left});
+    left = $(window).width() - $('#tooltip .chart').width();
+  }
+
   $('#tooltip').css({left: left, top: top}).children('.chart').highcharts(tooltip_options);
 }
 
