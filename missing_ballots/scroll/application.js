4f7db6464e64e514d0cce434eebdede277e88561
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-07-23 21:24:19 +0400

    w

--- missing_ballots/scroll/application.js
+++ missing_ballots/scroll/application.js
@@ -8,8 +8,15 @@ $(function ()
 
 
 
+  //controller.pin($('#page1 h1'), 500);
 
-//  controller.addTween(30, TweenMax.from($('section:eq(1) h1'), .5, {css: {rotation: -180}}));
+  controller.addTween($('#page1 h1').offset().top * .87, TweenMax.to($('#page1 h1'), .1, {css: {position: 'fixed', right: 0, left: 0, top: 0}}));
+
+  controller.addTween($('#page2').offset().top - 60, TweenMax.fromTo($('#page2 .middle'), .3, {css: {opacity: 0}}, {css: {opacity: 1}}).fromTo($('#page2 .top'), .3, {css: {opacity: 0}}, {css: {opacity: 1}}));
+//  controller.pin($('#page2'), 500);
+
+
+  //controller.addTween(30, TweenMax.from($('section:eq(1) h1'), .5, {css: {rotation: -180}}), 100);
 
 
 
