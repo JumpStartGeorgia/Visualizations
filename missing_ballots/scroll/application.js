da7b66f0237741cf1d5cfc1233437631ca546f13
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-08-01 05:00:22 +0400

    starting next section

--- missing_ballots/scroll/application.js
+++ missing_ballots/scroll/application.js
@@ -7,24 +7,50 @@ $(function ()
   });
 
 
-
-  controller.pin($('#page2'), 2500, {
+  var pinh = 2500;
+  controller.pin($('#page2'), pinh, {
     anim: (new TimelineMax())
           .append(TweenMax.to($('#page2 .bottom'), .5, {css:{top: 0}}))
-          .append(TweenMax.to($('#page2 .top'), .5, {css:{opacity: 1}}))
+          .append(TweenMax.to($('#page2 .top'), .5, {css:{opacity: 1}})),
+    onPin: function ()
+    {
+      $('#page2-spacer').hide();
+    },
+    onUnpin: function ()
+    {
+      if ($('#page2').offset().top > $(window).scrollTop())
+      {
+        $('#page2-spacer').show();
+      }
+    }
   });
+  $('#page2').after('<div id="page2-spacer"></div>').next().height(pinh);
+
+
 
   $('#ballots .all').css({right: $(window).width() - $('#ballots .all').offset().left - $('#ballots .all').width()});
   controller.pin($('#ballots .all'), 9999, {
     pushFollowers: false,
   });
 
-  var i = 9;
+  var i = $('#ballots .list li').length;
   while (i --)
   {
-    controller.addTween($('#ballots .list li').eq(i).children('.mark'), TweenMax.to($('#ballots .all li').eq(i), .001, {css: {visibility: 'visible'}}));
+    controller.addTween(
+      $('#ballots .list li').eq(i).children('.mark'),
+      (new TimelineMax()).append([
+        TweenMax.to($('#ballots .list li').eq(i).children('.mark'), .001, {css: {visibility: 'hidden'}}),
+        TweenMax.to($('#ballots .all li').eq(i), .001, {css: {visibility: 'visible'}})
+      ])
+    );
   }
   
+
+
+  $(window).load(function ()
+  {
+    //setTimeout(function (){ $(window).scroll(); }, 10);
+  });
 
 
 
