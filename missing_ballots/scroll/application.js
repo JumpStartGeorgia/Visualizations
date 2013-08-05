ca500af36ef06882a762828883547caf6eb6f07b
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-08-05 15:54:44 +0400

    added new elements and features

--- missing_ballots/scroll/application.js
+++ missing_ballots/scroll/application.js
@@ -52,6 +52,47 @@ $(function ()
   
 
 
+
+
+
+
+
+
+
+
+  var pinh = 3000;
+  controller.pin($('#page5'), pinh, {
+    anim: (new TimelineMax())
+          .append(TweenMax.fromTo($('#stations').parent(), .5, {css: {opacity: 0}}, {css: {opacity: 1}}))
+          .append(TweenMax.fromTo($('#stations-connect'), .5, {css:{top: 700}}, {css:{top: 0}}))
+          .append(TweenMax.to($('#stations-connect h5'), .5, {css:{top: -500}}))
+          .append(TweenMax.fromTo($('#stations-connect .img img:last-of-type, #stations-connect .right'), .5, {css:{top: 700}}, {css:{top: 0}})),
+    onPin: function ()
+    {
+      $('#page5-spacer').hide();
+    },
+    onUnpin: function ()
+    {
+      if ($('#page5').offset().top > $(window).scrollTop())
+      {
+        $('#page5-spacer').show();
+      }
+    }
+  });
+  $('#page5').after('<div id="page5-spacer"></div>').next().height(pinh);
+
+
+
+
+
+
+
+
+
+
+
+
+
   $(window).load(function ()
   {
     //setTimeout(function (){ $(window).scroll(); }, 10);
