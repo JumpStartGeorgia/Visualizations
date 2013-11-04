102ea7ff686d3a4887f9eae3b3bb6e5685b3c147
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-11-04 15:32:16 +0400

    automatic transition added

--- map_transition/ka/js/application.js
+++ map_transition/ka/js/application.js
@@ -3,16 +3,30 @@ $(function ()
 
 
 
+  function change (direction)
+  {
+    var activei = steps.filter('.active').removeClass('active').index();
+    var nexti = direction == 'left' ? (activei == 0 ? steps.length - 1 : activei - 1) : (activei == steps.length - 1 ? 0 : activei + 1);
+    steps.eq(nexti).addClass('active');
+  }
+
+
+  function automatic ()
+  {
+    timer = setTimeout(automatic, 5000);
+    change('right');
+  }
+
+  var timer = setTimeout(automatic, 5000);
+
+
   var steps = $('#object .step');
 
   $('#buttons .btn').click(function ()
   {
-    var activei = steps.filter('.active').removeClass('active').index();
-    var nexti = $(this).hasClass('left') ? (activei == 0 ? steps.length - 1 : activei - 1) : (activei == steps.length - 1 ? 0 : activei + 1);
-    steps.eq(nexti).addClass('active');
+    clearTimeout(timer);
+    change($(this).hasClass('left') ? 'left' : 'right');
   });
-
-
 
 
 
