6fd9854bd50314d1a2db9b9676ddcc736e9f8669
antarya <antarya@gmail.com>
2015-06-19 15:08:36 +0400

    Lari application currency get

--- household_income/assets/js/m.js
+++ household_income/assets/js/m.js
@@ -334,9 +334,10 @@ var mw = (function () {
                     .attr("width", map_w)
                     .attr("height", map_h);
 
-                    d3.json("http://lari.jumpstart.ge/en/api/v1/nbg_rates?currency=USD&start_date=" + Date.now(), function(error, shapes) {
-                      console.log('here',shapes);
-                    });
+                    // d3.jsonp("http://lari.jumpstart.ge/en/api/v1/nbg_rates?currency=USD&start_date=" + Date.now() + "&end_date=" + Date.now(), function() {
+                    //   // console.log('here',d);
+                    // });
+
 
     d3.json("assets/data/tbilisi.json", function(error, shapes) {
       if (error) throw error;
@@ -519,3 +520,30 @@ var mw = (function () {
   return obj;
 
 })();
+
+
+d3.jsonp = function (url, callback) {
+  function rand() {
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+      c = '', i = -1;
+    while (++i < 15) c += chars.charAt(Math.floor(Math.random() * 52));
+    return c;
+  }
+
+  function create(url) {
+    var e = url.match(/callback=d3.jsonp.(\w+)/),
+      c = e ? e[1] : rand();
+    d3.jsonp[c] = function(data) {
+      callback(data);
+      delete d3.jsonp[c];
+      script.remove();
+    };
+    return 'd3.jsonp.' + c;
+  }
+
+  var cb = create(url),
+    script = d3.select('head')
+    .append('script')
+    .attr('type', 'text/javascript')
+    .attr('src', url.replace(/(\{|%7B)callback(\{|%7D)/, cb));
+};
