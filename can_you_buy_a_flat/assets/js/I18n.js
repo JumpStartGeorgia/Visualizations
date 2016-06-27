ed1b1e333854fb6e119d6950ad709c1e3192c442
Antarya <antarya@gmail.com>
2016-06-27 11:15:17 +0400

    Mime type changed to application/javascript

--- can_you_buy_a_flat/assets/js/I18n.js
+++ can_you_buy_a_flat/assets/js/I18n.js
@@ -136,7 +136,7 @@ var I18n = (function () {
   };
   var load_file = function(src, callback, error_callback) {
      var s = document.createElement("script");
-     s.type = 'text/javascript';
+     s.type = 'application/javascript';
      s.src = src;
      s.async = false;
      s.onreadystatechange = s.onload = function () {
